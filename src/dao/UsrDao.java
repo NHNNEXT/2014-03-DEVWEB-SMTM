d53a90e61aff44e1ff17d907e03ee98060ae93bc
이경륜 <erin314@naver.com>
2015-01-03 10:26:10 +0900

    #34 dao result set 축약

--- src/dao/UsrDao.java
+++ src/dao/UsrDao.java
@@ -9,22 +9,24 @@ import entity.Usr;
 
 public class UsrDao {
 	public Usr findUsrById(final String usrId) {
-		RowMapper<Usr> rm = new RowMapper<Usr>(){
-			public Usr mapRows(ResultSet rs) throws SQLException {
-				return new Usr(rs.getString("USR_SEQ"), rs.getString("USR_ID"), 
-						rs.getString("USR_PW"),rs.getString("USR_NM"), rs.getString("USR_TYPE"),rs.getString("USR_PHONE1"),rs.getString("USR_GENDER"),rs.getString("USR_BIRTH"));
-			}
-		};
+		RowMapper<Usr> rm = resultSetOfUsr();
 		JdbcTemplate jdbcTemplate = new JdbcTemplate();
 		String sql = "SELECT USR_SEQ ,USR_ID, USR_NM, USR_PW, USR_TYPE, USR_PHONE1, USR_GENDER, USR_BIRTH FROM TB_USR WHERE USR_ID=?";	
 		return jdbcTemplate.executeQuery(sql, rm, usrId);
 	}
-	
 	public int register(final Usr usr) {
 		String currentMethod = new Object() {}.getClass().getEnclosingMethod().getName();
 		JdbcTemplate template = new JdbcTemplate();
 		String sql = "INSERT INTO TB_USR(USR_ID, USR_NM, USR_PW, USR_TYPE, USR_PHONE1, USR_GENDER, USR_BIRTH, CREATE_USR) VALUES(?,?,?,?,?,?,?,?)";	
 		return template.executeUpdate(sql, usr.getId(), usr.getName(), usr.getPw(),
 				Integer.parseInt(usr.getType()), usr.getPhone1(), usr.getGender(), usr.getBirth(), currentMethod);
+	}
+	private RowMapper<Usr> resultSetOfUsr() {
+		return new RowMapper<Usr>(){
+			public Usr mapRows(ResultSet rs) throws SQLException {
+				return new Usr(rs.getString("USR_SEQ"), rs.getString("USR_ID"), 
+						rs.getString("USR_PW"),rs.getString("USR_NM"), rs.getString("USR_TYPE"),rs.getString("USR_PHONE1"),rs.getString("USR_GENDER"),rs.getString("USR_BIRTH"));
+			}
+		};
 	}
 }
--- src/dao/WorkDao.java
+++ src/dao/WorkDao.java
@@ -10,14 +10,14 @@ import entity.Usr;
 import entity.Work;
 
 public class WorkDao {
-	public int goToWork(final Usr usr, final String storeSeq) {
+	public int insertGoToWork(final Usr usr, final String storeSeq) {
 		String currentMethod = new Object() {}.getClass().getEnclosingMethod().getName();
 		JdbcTemplate jdbcTemplate = new JdbcTemplate();
 		String sql = "INSERT INTO TB_WORK(WRK_STO_SEQ, WRK_ALBA_SEQ, WRK_START, CREATE_USR) VALUES(?,?,NOW(),?)";	
 		return jdbcTemplate.executeUpdate(sql, Integer.parseInt(storeSeq), usr.getSeq(), currentMethod);
 	}
 	
-	public int leaveWork(final Usr usr, final String storeSeq) {
+	public int insertLeaveWork(final Usr usr, final String storeSeq) {
 		String currentMethod = new Object() {}.getClass().getEnclosingMethod().getName();
 		JdbcTemplate jdbcTemplate = new JdbcTemplate();
 		String sql = "UPDATE TB_WORK SET WRK_STUS=1002, WRK_FINISH=NOW(), UPDATE_USR=? WHERE WRK_STUS=1001 AND WRK_STO_SEQ=? AND WRK_ALBA_SEQ=?";	
@@ -25,17 +25,20 @@ public class WorkDao {
 	}
 		
 	public ArrayList<Work> selectWork(final String storeSeq) {
-		RowMapper<Work> rm = new RowMapper<Work>(){
-			public Work mapRows(ResultSet rs) throws SQLException {
-				return new Work(rs.getString("WRK_SEQ"), rs.getString("WRK_STO_SEQ"), rs.getString("WRK_ALBA_SEQ"), rs.getString("WRK_STUS"), rs.getString("WRK_START"), rs.getString("WRK_FINISH"), rs.getString("USR_NM"));
-			}
-		};
-		
+		RowMapper<Work> rm = resultSetOfWork();
 		JdbcTemplate jdbcTemplate = new JdbcTemplate();
 		String sql = "SELECT W.WRK_SEQ, W.WRK_STO_SEQ, W.WRK_ALBA_SEQ, W.WRK_STUS, W.WRK_START, W.WRK_FINISH, U.USR_NM FROM TB_WORK W "
 				+ "JOIN TB_USR U "
 				+ "ON W.WRK_ALBA_SEQ = U.USR_SEQ WHERE (W.WRK_STO_SEQ = ? AND (W.WRK_STUS = 1001 OR W.WRK_STUS = 1002 OR W.WRK_STUS = 1003))";
 		return jdbcTemplate.executeQueryList(sql, rm, storeSeq);
+	}
+
+	private RowMapper<Work> resultSetOfWork() {
+		return new RowMapper<Work>(){
+			public Work mapRows(ResultSet rs) throws SQLException {
+				return new Work(rs.getString("WRK_SEQ"), rs.getString("WRK_STO_SEQ"), rs.getString("WRK_ALBA_SEQ"), rs.getString("WRK_STUS"), rs.getString("WRK_START"), rs.getString("WRK_FINISH"), rs.getString("USR_NM"));
+			}
+		};
 	}
 	
 	public ArrayList<Work> showWork(final Usr usr) {
@@ -83,18 +86,14 @@ public class WorkDao {
 		String sql = "UPDATE TB_WORK SET WRK_START_CONFIRM=NOW(), WRK_STUS=1003, UPDATE_USR=? WHERE WRK_SEQ=?";
 		return template.executeUpdate(sql, currentMethod, work.getSeq());
 	}
-
-	public int confirmBoth(final Work work) {
-		String currentMethod = new Object() {}.getClass().getEnclosingMethod().getName();
-		JdbcTemplate template = new JdbcTemplate();
-		String sql = "UPDATE TB_WORK SET WRK_START_CONFIRM=NOW(), WRK_FINISH_CONFIRM=NOW(), WRK_STUS=1004, UPDATE_USR=? WHERE WRK_SEQ=?";	
-		return template.executeUpdate(sql, currentMethod, work.getSeq());
-	}
-
 	public int confirmLeaveWork(final Work work) {
 		String currentMethod = new Object() {}.getClass().getEnclosingMethod().getName();
 		JdbcTemplate template = new JdbcTemplate();
 		String sql = "UPDATE TB_WORK SET WRK_FINISH_CONFIRM=NOW(), WRK_STUS=1004, UPDATE_USR=? WHERE WRK_SEQ=?";	
 		return template.executeUpdate(sql, currentMethod, work.getSeq());
 	}
+	public int confirmBoth(final Work work) {
+		 confirmGoToWork(work);
+		 return confirmLeaveWork(work);	
+		}
 }
