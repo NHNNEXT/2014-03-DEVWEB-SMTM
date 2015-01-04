71d8e1f3ffba6230176c25e0f19f1702a4b2bb60
이경륜 <erin314@naver.com>
2015-01-04 17:04:51 +0900

    #2 로그인  패스워드 암호화 클래스 만듬

---
+++ src/register/SHA256Encrypt.java
@@ -0,0 +1,28 @@
+package register;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+
+public class SHA256Encrypt {
+	public static String encrypt(String value) {
+		String sha ="";
+		MessageDigest sh = null;
+		
+		try {
+			sh = MessageDigest.getInstance("SHA-256");
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+		}
+		sh.update(value.getBytes());
+		byte byteData[] = sh.digest();
+		StringBuffer sb = new StringBuffer();
+		
+		for(int i = 0 ; i < byteData.length ; i++){
+			sb.append(Integer.toString((byteData[i]&0xff) + 0x100, 16).substring(1));
+		}
+		
+		sha = sb.toString();
+		return sha;
+	}
+}
